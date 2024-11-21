0946f3669c8ee5ed31fd80e49be97e19b0124d19
Invincible-Atlas <148077776+Invincible-Atlas@users.noreply.github.com>
2024-11-21 11:17:16 -0500

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,2 +1,21 @@
-
-
+fetch("config/pages.json").then((res) => {
+  if (res.ok) {
+    return (res.json());
+  } else {
+    console.error("navbar.js: Fetch error " + res.status);
+  }
+}).then((json)=>{
+  const navbarContainer = document.getElementsByClassName("navbarTop")[0];
+  if(navbarContainer!==""&&navbarContainer!==null&&navbarContainer!==undefined){
+    json.forEach((value,index,array) => {
+      let navlink = document.createElement("a");
+      navlink.classList="navlink navlinkTop";
+      navlink.innerHTML = value.name;
+      navlink.href = value.link;
+      navbarContainer.appendChild(navlink);
+    });
+  }else{
+    console.error("navbar.js: No navbar container")
+  }
+    
+})
